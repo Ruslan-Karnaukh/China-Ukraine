cdae316d7b1ece30242cf7d1ae63c72bde383cc4
Ruslan-Karnaukh <115695997+Ruslan-Karnaukh@users.noreply.github.com>
2023-12-15 16:13:08 +0000

    Deploying to gh-pages from @ Ruslan-Karnaukh/china-ukraina@35219f67903d403558b1b185e7b888625204f2b4 🚀

---
+++ index.10e72ce3.js
@@ -0,0 +1,2 @@
+const i=document.querySelector(".btn-scroll");document.addEventListener("scroll",(()=>{window.scrollY>400?i.style.visibility="visible":i.style.visibility="hidden"})),i.addEventListener("click",(()=>{window.scrollTo({top:0,behavior:"smooth"})}));
+//# sourceMappingURL=index.10e72ce3.js.map
